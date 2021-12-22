7cdb470371679990f05eec356d943ddc0d83965f
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-22 13:51:02 +0900

    ADD - 모델 생성

--- server/lowest/models.py
+++ server/lowest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Brands(models.Model):
+    ko_name = models.CharField(max_length=64)
+    en_name = models.CharField(max_length=64)
+    url = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
+    created_date = models.DateTimeField()
+
+
+class History(models.Model):
+    product = models.ForeignKey('Products', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_date = models.DateTimeField()
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=128)
+    brand = models.ForeignKey('Brands', on_delete=models.SET_NULL, null=True)
+    current = models.ForeignKey(
+        'History', on_delete=models.SET_DEFAULT, default=0, related_name='current')
+    lowest = models.ForeignKey(
+        'History', on_delete=models.SET_DEFAULT, default=0, related_name='lowest')
+    highest = models.ForeignKey(
+        'History', on_delete=models.SET_DEFAULT, default=0, related_name='highest')
+    url = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
+    created_date = models.DateTimeField()
