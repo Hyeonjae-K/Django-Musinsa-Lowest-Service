b8341547e8bcbe6ce70170683d25628ddeb6886f
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-22 16:37:50 +0900

    FEAT - 테스트 뷰 추가

--- server/lowest/views.py
+++ server/lowest/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from models import Brands, History, Products
+from serializers import BrandsSerializer, HistorySerializer, ProductsSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def testBrands(request):
+    total_brands = Brands.objects.all()
+    serializer = BrandsSerializer(total_brands, many=True)
+    Response(serializer.data)
+
+
+@api_view(['GET'])
+def testHistory(request):
+    total_History = History.objects.all()
+    serializer = HistorySerializer(total_History, many=True)
+    Response(serializer.data)
+
+
+@api_view(['GET'])
+def testProducts(request):
+    total_Products = Products.objects.all()
+    serializer = ProductsSerializer(total_Products, many=True)
+    Response(serializer.data)
