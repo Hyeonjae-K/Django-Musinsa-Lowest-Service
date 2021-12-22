cae38c62ccd476d0303fc92fbb12c999f037bc13
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-22 16:55:50 +0900

    ADD - admin 페이지 모델 추가

--- server/lowest/admin.py
+++ server/lowest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Brands, History, Products
 
-# Register your models here.
+admin.site.register(Brands)
+admin.site.register(History)
+admin.site.register(Products)
