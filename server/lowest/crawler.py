7aff42a5c686d3ad528b0f19c8978a30b892175d
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-23 00:49:26 +0900

    FIX - 오타 수정 및 예외처리

--- server/lowest/crawler.py
+++ server/lowest/crawler.py
@@ -1,21 +1,18 @@
 import django
 django.setup()
 
-import requests
-from bs4 import BeautifulSoup
-from functools import partial
-from multiprocessing import Pool, cpu_count
-from django.utils import timezone
 from .models import Brands, History, Products
-
+from django.utils import timezone
+from multiprocessing import Pool, cpu_count
+from functools import partial
+from bs4 import BeautifulSoup
+import requests
 
 headers = {'User-Agent': 'Mozila/5.0'}
-display_url = 'https://display.musinsa.com/display/brands/%s/goods?page=%d'
 
 
 def _brandsCrawler(page_num, brand):
-    soup = BeautifulSoup(requests.get(display_url %
-                         (brand.url, page_num)).text, 'html.parser')
+    soup = BeautifulSoup(requests.get(brand.url + f'?page={page_num}').text, 'html.parser')
     prices = [int(elem.text.split()[-1].replace(',', '').replace('원', ''))
               for elem in soup.select('#searchList > li > div.li_inner > div.article_info > p.price')]
     images = ['https:' + elem['data-original']
@@ -26,11 +23,10 @@ def _brandsCrawler(page_num, brand):
     names = [elem['title'] for elem in elems]
     urls = ['https:' + elem['href'] for elem in elems]
 
-    histories, update_products, new_products = [], [], []
+    histories, products = [], []
     datetime = timezone.now()
     for price, image, product_id, name, url in zip(prices, images, product_ids, names, urls):
-        history = History(product=product_id, price=price,
-                          created_date=datetime)
+        history = History(product=product_id, price=price, created_date=datetime)
         histories.append(history)
         try:
             product = Products.objects.get(id=product_id)
@@ -39,14 +35,18 @@ def _brandsCrawler(page_num, brand):
                 product.lowest = history
             if product.highest.price < history.price:
                 product.highest = history
-            update_products.apppend(product)
+            products.append(product)
         except:
-            product = Products(id=product_id, name=name, brand=brand, current=history,
-                               lowest=history, highest=history, image=image, created_date=datetime)
-            new_products.append(product)
+            try:
+                Products(id=product_id, name=name, brand=brand, current=history, lowest=history, highest=history, url=url, image=image, created_date=datetime).save()
+            except:
+                continue
+    
     History.objects.bulk_create(histories)
-    Products.objects.bulk_create(new_products)
-    Products.objects.bulk_update(update_products)
+    try:
+        Products.objects.bulk_update(products)
+    except:
+        pass
 
 
 def brandsCrawler():
