5016dc24d9d5ca5a8bde5c65956e81ef3fe49e7d
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-22 16:39:56 +0900

    ADD - 테스트 url 추가

---
+++ server/lowest/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from views import testBrands, testHistory, testProducts
+
+urlpatterns = [
+    path('test/brands/', testBrands),
+    path('test/history/', testHistory),
+    path('test/products/', testProducts),
+]
