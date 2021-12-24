6b5f8f74666d44a3922c2625d12e2948d0623e00
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-24 13:53:14 +0900

    ADD - api url 추가

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('lowest/', include('lowest.urls')),
 ]
