c68ad0c4b839f830e860237e8b89045ad90f4bb5
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-24 19:55:51 +0900

    FEAT - filter 기능 추가

--- server/lowest/viewsets.py
+++ server/lowest/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Brands, History, Products
 from .serializers import BrandsSerializer, HistorySerializer, ProductsSerializer
 
@@ -6,13 +7,19 @@ from .serializers import BrandsSerializer, HistorySerializer, ProductsSerializer
 class BrandsViewSet(viewsets.ModelViewSet):
     queryset = Brands.objects.all()
     serializer_class = BrandsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
 
 
 class HistoryViewSet(viewsets.ModelViewSet):
     queryset = History.objects.all()
     serializer_class = HistorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
 
 
 class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
