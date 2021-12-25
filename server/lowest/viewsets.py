7aea3c8bfc62d9e88377bb4ec3dda9e85e7688f8
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-25 22:31:24 +0900

    FEAT - paging 기능 추가

--- server/lowest/viewsets.py
+++ server/lowest/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 from .models import Brands, History, Products
 from .serializers import BrandsSerializer, HistorySerializer, ProductsSerializer
 
@@ -12,14 +13,19 @@ class BrandsViewSet(viewsets.ModelViewSet):
 
 
 class HistoryViewSet(viewsets.ModelViewSet):
-    queryset = History.objects.all()
+    queryset = History.objects.all().order_by('-created_date')
     serializer_class = HistorySerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
 
 
+class ProductsSetPagination(PageNumberPagination):
+    page_size = 30
+
+
 class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
+    pagination_class = ProductsSetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
